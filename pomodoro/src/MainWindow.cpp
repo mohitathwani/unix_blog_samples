99ce702d5e1f793bf6cad6ff9eb4777f4f2c4845
Mohit Athwani <mohit@athwani.net>
2019-06-16 10:22:45 -0700

    Using resources .xml to load resources

--- pomodoro/src/MainWindow.cpp
+++ pomodoro/src/MainWindow.cpp
@@ -4,13 +4,15 @@ MainWindow::MainWindow() {
     set_title("Pomodoro");
     set_name("MainWindow");
     set_resizable(false);
-    set_default_size(320, 600);
+    // set_default_size(320, 600);
+    set_default_size(600,600);
     set_position(Gtk::WIN_POS_CENTER);
 
     m_css_provider = Gtk::CssProvider::create();
     try
     {
-        m_css_provider->load_from_path("/usr/local/share/pomodoro/MainWindow.css");
+        // m_css_provider->load_from_path("/usr/local/share/pomodoro/MainWindow.css");
+        m_css_provider->load_from_resource("/stylesheets/MainWindow.css");
     }
     catch(const Glib::Error& e)
     {
@@ -20,7 +22,8 @@ MainWindow::MainWindow() {
     m_screen = Gdk::Screen::get_default();
     m_style_context = get_style_context();
     m_style_context->add_provider_for_screen(m_screen, m_css_provider, GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
-    add(m_main_grid);
+    // add(m_main_grid);
+    add(m_clock_view);
 
     show_all();
 }
